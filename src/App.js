38b8e26c30c3aa22a2a88a6ad180d8eb7806aef5
Daniel De La Paz <danieldlpxxx@gmail.com>
2019-08-05 11:28:49 -0500

    primer commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <section className="App">
+      <nav className="Navegacion"></nav>
+      <aside className="Canales"></aside>
+      <section className="Contenido">
+        <header className="Busqueda"></header>
+        <section className="Chat"></section>
+        <footer className="Ingreso"></footer>
+      </section>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/Navegacion/Navegacion.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Navegacion.css';
+
+const Navegacion = (props) => (
+    <div></div>
+);
+
+export default Navegacion;
